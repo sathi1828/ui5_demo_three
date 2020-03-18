73a81c8486750dc8ccd14d2697a0f352c66b63e9
sathi1828 <sathishsathi2010@gmail.com>
2020-03-18 17:57:51 +0530

    calling a odata service through the descriptor file(manifest.json)

--- webapp/controller/login/Login.controller.js
+++ webapp/controller/login/Login.controller.js
@@ -7,8 +7,12 @@ sap.ui.define([
  
     return BaseController.extend("demo.three.controller.login.Login", {
          onInit:function(){
-            var oModel = new ODataModel("http://localhost:8081/services.odata.org/Northwind/Northwind.svc/");
-            console.log('omodel', oModel)
+            // var oModel = new ODataModel("http://localhost:8081/services.odata.org/Northwind/Northwind.svc/");
+            // console.log('omodel', oModel)
+            
+            this.oNorthWindModel = this.getOwnerComponent().getModel("northWind");
+            console.log('model is',this.oNorthWindModel);
+            let oView = this.getView().setModel(this.oNorthWindModel);
          }
     });
  
